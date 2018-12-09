02f0bf8d29bc5904037d342ec04e903912951041
David Montgomery <david.montgomery@live.se>
2018-12-09 12:59:56 +0100

    feature tracking

--- slam.py
+++ slam.py
@@ -1,17 +1,33 @@
 #!/usr/bin/env python
 
 import cv2 
-
+import numpy as np
 from display import Display
+from extractor import Extractor
 
 W, H = 1920//2, 1080//2
+disp = Display(W, H)
+
+
+
+fe = Extractor()
 
 def process_frame(img):
     img = cv2.resize(img, (W, H))
-    display.paint(img)
+    matches = fe.extract(img)
+
+    print("Num matches: %i" % len(matches))
+
+    for pt1, pt2 in matches:
+        u1, v1 = map(lambda x: int(round(x)), pt1) 
+        u2, v2 = map(lambda x: int(round(x)), pt2)
+        cv2.circle(img, (u1,v1), color=(0,255,0), radius=3)
+        cv2.line(img, (u1,v1), (u2,v2), color=(255,0,0))
+
+    disp.paint(img)
     
 if __name__ == "__main__":
-    display = Display(W, H)
+    
     cap = cv2.VideoCapture("videos/fastcar.mp4")
 
     while cap.isOpened():
