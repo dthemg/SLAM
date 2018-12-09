3d78b51cd8bdda1a2dab0b63b7f81493dbb271b4
David Montgomery <david.montgomery@live.se>
2018-12-09 14:04:05 +0100

    improved features

--- extractor.py
+++ extractor.py
@@ -1,14 +1,33 @@
 import cv2 
 import numpy as np
+
+np.set_printoptions(suppress=True)
+
 from skimage.measure import ransac
 from skimage.transform import FundamentalMatrixTransform
+from skimage.transform import EssentialMatrixTransform
+
+
+def add_ones(x):
+    return np.concatenate([x, np.ones((x.shape[0], 1))], axis=1)
 
 class Extractor:
     
-    def __init__(self):
+    def __init__(self, K):
         self.orb = cv2.ORB_create()
         self.bf = cv2.BFMatcher(cv2.NORM_HAMMING)
         self.last = None
+        self.K = K
+        self.Kinv = np.linalg.inv(self.K)
+
+    def normalize(self, pts):
+        return np.dot(self.Kinv, add_ones(pts).T).T[:,0:2]
+        
+        
+
+    def denormalize(self, pt):
+        ret = np.dot(self.K, np.array([pt[0],pt[1],1.0]))
+        return int(round(ret[0])), int(round(ret[1]))
 
     def extract(self, img):
         # detection
@@ -29,15 +48,25 @@ class Extractor:
                     kp2 = self.last['kps'][m.trainIdx].pt
                     ret.append((kp1, kp2))
 
+        
+        
         # filter    
         if len(ret) > 0:
             ret = np.array(ret)
+
+            # centralize coordinates
+            ret[:,0,:] = self.normalize(ret[:,0,:])
+            ret[:,1,:] = self.normalize(ret[:,1,:])
+
             model, inliners = ransac((ret[:,0], ret[:,1]),
+                                    #EssentialMatrixTransform,
                                     FundamentalMatrixTransform,
                                     min_samples=8, 
                                     residual_threshold=1,
                                     max_trials=100)
             ret = ret[inliners]
+            s,v,d = np.linalg.svd(model.params)
+
         # return
         self.last = {'kps': kps, 'des': des}
         return ret
